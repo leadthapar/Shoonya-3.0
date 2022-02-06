aee831ecf474b5584707951ee50b8fdc64effb9b
Rupinder <rupinderkaur8804@gmail.com>
2022-02-06 11:39:13 +0530

    Updated content

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -58,7 +58,7 @@ function Event() {
               imgPath={monopoly}
               isBlog={false}
               title="Triwizard Tournament"
-              description="This will be a day-long coding competition held in teams of either 1 or 2 members. Every round will be a knockout round introducing the participants to a new aspect of coding. Round 1 will be a mystery language round where participants will be assigned a mystery language and will have to solve three questions based on that. Round 2 will be a debugging round which will act as a hint for the last round. "
+              description="This will be a day-long coding competition held in teams of either 1 or 2 members. Every round will be a knockout round introducing the participants to a new aspect of coding. Round 1 will be a mystery language round where participants will be assigned a mystery language and will have to solve three questions based on that. Round 2 will be a debugging round which will act as a hint for the last round. Date : 28th Feb 2022"
               />
               <br></br>
               <Button onClick={openModal1}>Register</Button>
@@ -71,7 +71,7 @@ function Event() {
               imgPath={ctf}
               isBlog={false}
               title="Chamber of Secrets"
-              description="This will be a time-based team CTF where teams of 3-4 members will be provided with a particular problem. Individual registrations are allowed, in which case the organizing committee will form teams. After solving, a clue will be generated which will direct the teams to a specific piece of the story. In the end, after clubbing all the clues, an account of a synchronous story will be formed. The teams with the most appropriate stories would be declared the winners."
+              description="This will be a time-based team CTF where teams of 3-4 members will be provided with a particular problem. Individual registrations are allowed, in which case the organizing committee will form teams. After solving, a clue will be generated which will direct the teams to a specific piece of the story. In the end, after clubbing all the clues, an account of a synchronous story will be formed. The teams with the most appropriate stories would be declared the winners. Date : 1st March 2022"
               
             />
             <br></br>
@@ -85,7 +85,7 @@ function Event() {
               imgPath={journaling}
               isBlog={false}
               title="Weasly’s Pitchers"
-              description="This event will be a pitching event held in teams of either 1 or 2 members. Every team will be given a product to sell and their pitch will revolve around the same.Teams are supposed to use a ppt or a poster in their pitch which would eventually exhibit their creativity and presentation skills"
+              description="This event will be a pitching event held in teams of either 1 or 2 members. Every team will be given a product to sell and their pitch will revolve around the same.Teams are supposed to use a ppt or a poster in their pitch which would eventually exhibit their creativity and presentation skills. Date : 3rd March 2022"
               
             />
             <br></br>
--- src/components/Schedule/timeline.js
+++ src/components/Schedule/timeline.js
@@ -3,27 +3,27 @@ let timeline = [
     id: 1,
     title: "The Triwizard Tournament",
     description:
-      "This will be a day-long coding competition held in teams of either 1 or 2 members. Every round will be a knockout round introducing the participants to a new aspect of coding. Round 1 will be a mystery language round where participants will be assigned a mystery language and will have to solve three questions based on that. Round 2 will be a debugging round which will act as a hint for the last round.",
+      "Unleash your technical mind and explore your searching skills. Find the problem, then wrap it up and climb the rounds. Isn't it simple?",
     buttonText: "View Frontend Projects",
-    date: "TBA",
+    date: "28th Feb 2022",
     icon: "work",
   },
   {
     id: 2,
     title: "Chamber of Secrets",
     description:
-      "This will be a time-based team CTF where teams of 3-4 members will be provided with a particular problem. Individual registrations are allowed, in which case the organizing committee will form teams. After solving, a clue will be generated which will direct the teams to a specific piece of the story. In the end, after clubbing all the clues, an account of a synchronous story will be formed. The teams with the most appropriate stories would be declared the winners.",
+      "Unravel the mystery and join the broken pieces to move ahead in the game. Patch the logic to complete the story.",
     buttonText: "View Backend Projects",
-    date: "TBA",
+    date: "1st March 2022",
     icon: "work",
   },
   {
     id: 3,
     title: "Weasly’s Pitchers",
     description:
-      "This event will be a pitching event held in teams of either 1 or 2 members. Every team will be given a product to sell and their pitch will revolve around the same.Teams are supposed to use a ppt or a poster in their pitch which would eventually exhibit their creativity and presentation skills",
+      "We present you with a pitching war filled with imaginative ideas to compete. Showcase your creativity and boast with your speaking skills.",
       buttonText: "Company Website",
-    date: "TBA",
+    date: "3rd March 2022",
     icon: "work",
   },
 ]
